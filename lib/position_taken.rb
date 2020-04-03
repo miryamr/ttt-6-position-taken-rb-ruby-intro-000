9de07f2e91ee0b35e9220717bb81232076df6924
Miryam Rabinowitz <miryamrabinowitz@gmail.com>
2020-04-03 22:40:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  elsif board[index] != " " || "" || nil
+  elsif board[index] > " " || "" || nil
     return true
 end
 end
